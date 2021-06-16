f1e223b9633abb8178df8c48ec2688d6be99c150
katerina-spb <katespb98@gmail.com>
2021-06-16 16:17:38 -0500

    Added dummy data

---
+++ src/contactsData.js
@@ -0,0 +1,40 @@
+var contactsData = [
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    number: 7771231212,
+    email: 'john@gmail.com'
+  },
+  {
+    firstName: 'Jim',
+    lastName: 'Clark',
+    number: 7171231213,
+    email: 'jim@gmail.com'
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Black',
+    number: 7271231244,
+    email: 'jane@gmail.com'
+  },
+  {
+    firstName: 'Matt',
+    lastName: 'White',
+    number: 7371231215,
+    email: 'matt@gmail.com'
+  },
+  {
+    firstName: 'David',
+    lastName: 'Johns',
+    number: 7471231216,
+    email: 'david@gmail.com'
+  },
+  {
+    firstName: 'Alice',
+    lastName: 'Miles',
+    number: 7571231217,
+    email: 'alice@gmail.com'
+  }
+];
+
+export default contactsData;
