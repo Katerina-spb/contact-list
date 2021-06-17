9d22795f83a4a66b46fafdc60e378a99d5d503fd
katerina-spb <katespb98@gmail.com>
2021-06-16 23:42:36 -0500

    Added button to expand the view for a single contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,30 @@
 import './Contact.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Contact(props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = (event) => {
+    if (expanded === false) {
+      event.target.innerHTML = 'Hide info';
+    } else {
+      event.target.innerHTML = 'Show more info';
+    }
+    setExpanded(!expanded);
+  }
+
   const confirmDelete = () => {
     if (window.confirm(`Are you sure you want to delete the contact for ${props.contact.firstName}?`)) {
       props.delete(props.contact.id);
     }
+  }
+
+  let email;
+  if (expanded) {
+    email = <li>Email: {props.contact.email}</li>;
+  } else {
+    email = '';
   }
 
   return (
@@ -25,6 +44,8 @@ function Contact(props) {
         <li>Fist Name: {props.contact.firstName}</li>
         <li>Last Name: {props.contact.lastName}</li>
         <li>Phone number: {props.contact.number}</li>
+        {email}
+        <li><button onClick = {handleExpand}>Show more info</button></li>
       </ul>
     </div>
   )
